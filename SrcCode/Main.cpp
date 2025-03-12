0cdf0c28a20917b945cdd3272b09daafa8eb8552
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-12 18:57:20 +0300

    ADD: High-level entry point

---
+++ SrcCode/Main.cpp
@@ -0,0 +1,16 @@
+/*
+	Source code of ActOfRose global script execution system. Viktoriia Pashchenko. 2025.
+	
+	This file is part of system source code.
+	
+	@Name:			Main.cpp
+	@Created:		12.03.2025
+	@Programmer:	Viktoriia Pashchenko (SweetyDarkyRosy)
+	
+	High-level entry point. */
+
+// High-level entry point
+int main(int argc, char* argv[])
+{
+	return 0;
+}
