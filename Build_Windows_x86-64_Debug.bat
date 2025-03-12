9c89649f3bea4920a8cc899fb0b52eb3cd1d5ee7
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-12 20:05:09 +0300

    ADD: Windows-specific scripts for building ActOfRose application

---
+++ Build_Windows_x86-64_Debug.bat
@@ -0,0 +1,21 @@
+@echo off
+
+call "%~dp0\Build\Windows\SetVSDevEnv_x86-64.cmd"
+if %ERRORLEVEL% EQU 0 goto COMPILE_AND_BUILD
+
+:FAIL
+echo Visual Studio Environment detection failed.
+exit /b 1
+
+:COMPILE_AND_BUILD
+if exist "Bin" (
+    rd /s /q "Bin"
+)
+
+mkdir "Bin\Intermediates\Win_x86-64\Debug"
+
+for %%f in ("SrcCode\*.cpp") do (
+    cl.exe /c "%%f" /Fo"Bin\Intermediates\Win_x86-64\Debug\%%~nf.obj" /Fd"Bin\Intermediates\Win_x86-64\Debug\%%~nf.pdb" /W3 /Od /std:c++17 /Zi /MTd /DDEBUG /D_DEBUG
+)
+
+link.exe /DEBUG:FULL /MACHINE:X64 "Bin\Intermediates\Win_x86-64\Debug\*.obj" /OUT:"Bin\ActOfRose.exe"
---
+++ Build_Windows_x86-64_Release.bat
@@ -0,0 +1,21 @@
+@echo off
+
+call "%~dp0\Build\Windows\SetVSDevEnv_x86-64.cmd"
+if %ERRORLEVEL% EQU 0 goto COMPILE_AND_BUILD
+
+:FAIL
+echo Visual Studio Environment detection failed.
+exit /b 1
+
+:COMPILE_AND_BUILD
+if exist "Bin" (
+    rd /s /q "Bin"
+)
+
+mkdir "Bin\Intermediates\Win_x86-64\Release"
+
+for %%f in ("SrcCode\*.cpp") do (
+    cl /c "%%f" /Fo"Bin\Intermediates\Win_x86-64\Release\%%~nf.obj" /Fd"Bin\Intermediates\Win_x86-64\Release\%%~nf.pdb" /W3 /O2 /std:c++17 /Zi /MT /DNDEBUG /D_NDEBUG
+)
+
+link.exe /DEBUG:FULL /MACHINE:X64 "Bin\Intermediates\Win_x86-64\Release\*.obj" /OUT:"Bin\ActOfRose.exe"
