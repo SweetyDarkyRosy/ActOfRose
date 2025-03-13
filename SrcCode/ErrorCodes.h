c1eca2748aa92a14fa60b9407da52dd1ae96b8fe
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-13 21:03:51 +0300

    ADD: AOR_ERROR_SUCCESS code

---
+++ SrcCode/ErrorCodes.h
@@ -0,0 +1,17 @@
+/*
+	Source code of ActOfRose global script execution system. Viktoriia Pashchenko. 2025.
+	
+	This file is part of system source code.
+	
+	@Name:			ErrorCodes.h
+	@Created:		13.03.2025
+	@Programmer:	Viktoriia Pashchenko (SweetyDarkyRosy)
+	
+	Error codes. */
+
+#ifndef __ACT_OF_ROSE_ERROR_CODES_H__
+#define __ACT_OF_ROSE_ERROR_CODES_H__
+
+#define AOR_ERROR_SUCCESS						0
+
+#endif // !__ACT_OF_ROSE_ERROR_CODES_H__
