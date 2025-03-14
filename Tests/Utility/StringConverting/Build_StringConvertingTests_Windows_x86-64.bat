b5a9a6296744940df1c18fcb9d70161323e22320
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-14 10:31:28 +0300

    ADD: Build script for tests to StringConverting.h

---
+++ Tests/Utility/StringConverting/Build_StringConvertingTests_Windows_x86-64.bat
@@ -0,0 +1,17 @@
+@echo off
+
+call "%~dp0\..\..\..\Build\Windows\SetVSDevEnv_x86-64.cmd"
+if %ERRORLEVEL% EQU 0 goto COMPILE_AND_BUILD
+
+:FAIL
+echo Visual Studio Environment detection failed.
+exit /b 1
+
+:COMPILE_AND_BUILD
+
+cl.exe /c "..\..\..\SrcCode\Utility\StringConverting.cpp" /Fo"StringConverting.obj" /I"..\..\..\SrcCode" /W3 /Od /std:c++17 /MTd /DDEBUG /D_DEBUG
+cl.exe /c StringConvertingTests.cpp /Fo"StringConvertingTests.obj" /I"..\..\..\SrcCode" /W3 /Od /std:c++17 /MTd /DDEBUG /D_DEBUG
+
+link.exe /DEBUG:FULL /MACHINE:X64 "*.obj" /OUT:"StringConvertingTestsExec.exe"
+
+del *.obj
