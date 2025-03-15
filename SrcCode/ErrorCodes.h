69bc02962284dc700636c5ca162eb24ad582b9bd
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-15 20:24:55 +0300

    ADD: AOR_TOKEN_END_OF_SCRIPT return code

--- SrcCode/ErrorCodes.h
+++ SrcCode/ErrorCodes.h
@@ -12,7 +12,9 @@
 #ifndef __ACT_OF_ROSE_ERROR_CODES_H__
 #define __ACT_OF_ROSE_ERROR_CODES_H__
 
-#define AOR_SUCCESS						0			// Code of successfull execution
+#define AOR_SUCCESS								0			// Code of successfull execution
+
+#define AOR_TOKEN_END_OF_SCRIPT					1			// Code returned if the end of a script is reached
 
 #define AOR_ERROR_INVALID_PARAMETER				(-1)		// Error due to invalid parameter/parameters
 #define AOR_ERROR_INVALID_CHARACTER				(-2)		// Error due to invalid character (according to actual character/string encoding)
