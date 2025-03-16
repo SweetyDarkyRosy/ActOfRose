768cd0ce7acf5f678a9537aed530eae93dd488fc
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-16 14:51:36 +0300

    ADD: AOR_ERROR_TOKEN_INVALID_NUMBER_VALUE error code

--- SrcCode/ReturnCodes.h
+++ SrcCode/ReturnCodes.h
@@ -21,6 +21,7 @@
 #define AOR_ERROR_SCRIPT_FILE_NOT_LOADED			(-3)		// Error due to invalid path to a file with a script or internal error happened while opening a file
 #define AOR_ERROR_TOKEN_UNTERMINATED_STRING			(-4)		// Error due to unterminated/unquoted string when the end of a script is reached
 #define AOR_ERROR_TOKEN_INVALID_ESCAPE_SEQUENCE		(-5)		// Error due to invalid escape sequence found while string tokenisation
+#define AOR_ERROR_TOKEN_INVALID_NUMBER_VALUE		(-6)		// Error due to invalid number value
 
 
 #endif // !__ACT_OF_ROSE_ERROR_CODES_H__
