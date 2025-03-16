60a5d63719642174523da105f8c7a1fe53f9feb1
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-16 11:32:13 +0300

    ADD: AOR_ERROR_TOKEN_INVALID_ESCAPE_SEQUENCE error code

--- SrcCode/ReturnCodes.h
+++ SrcCode/ReturnCodes.h
@@ -12,14 +12,15 @@
 #ifndef __ACT_OF_ROSE_ERROR_CODES_H__
 #define __ACT_OF_ROSE_ERROR_CODES_H__
 
-#define AOR_SUCCESS								0			// Code of successfull execution
+#define AOR_SUCCESS									0			// Code of successfull execution
 
-#define AOR_TOKEN_END_OF_SCRIPT					1			// Code returned if the end of a script is reached
+#define AOR_TOKEN_END_OF_SCRIPT						1			// Code returned if the end of a script is reached
 
-#define AOR_ERROR_INVALID_PARAMETER				(-1)		// Error due to invalid parameter/parameters
-#define AOR_ERROR_INVALID_CHARACTER				(-2)		// Error due to invalid character (according to actual character/string encoding)
-#define AOR_ERROR_SCRIPT_FILE_NOT_LOADED		(-3)		// Error due to invalid path to a file with a script or internal error happened while opening a file
-#define AOR_ERROR_TOKEN_UNTERMINATED_STRING		(-4)		// Error due to unterminated/unquoted string when the end of a script is reached
+#define AOR_ERROR_INVALID_PARAMETER					(-1)		// Error due to invalid parameter/parameters
+#define AOR_ERROR_INVALID_CHARACTER					(-2)		// Error due to invalid character (according to actual character/string encoding)
+#define AOR_ERROR_SCRIPT_FILE_NOT_LOADED			(-3)		// Error due to invalid path to a file with a script or internal error happened while opening a file
+#define AOR_ERROR_TOKEN_UNTERMINATED_STRING			(-4)		// Error due to unterminated/unquoted string when the end of a script is reached
+#define AOR_ERROR_TOKEN_INVALID_ESCAPE_SEQUENCE		(-5)		// Error due to invalid escape sequence found while string tokenisation
 
 
 #endif // !__ACT_OF_ROSE_ERROR_CODES_H__
