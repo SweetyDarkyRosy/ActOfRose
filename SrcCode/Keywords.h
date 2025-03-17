48f14db8c5cf69c783a71a1e7f122460fbc1e946
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-17 09:27:48 +0300

    ADD: Enumeration of basic keywords

---
+++ SrcCode/Keywords.h
@@ -0,0 +1,36 @@
+/*
+	Source code of ActOfRose global script execution system. Viktoriia Pashchenko. 2025.
+	
+	This file is part of system source code.
+	
+	@Name:			Keywords.h
+	@Created:		17.03.2025
+	@Programmer:	Viktoriia Pashchenko (SweetyDarkyRosy)
+	
+	Set of keywords. */
+
+#ifndef __ACT_OF_ROSE_KEYWORDS_H__
+#define __ACT_OF_ROSE_KEYWORDS_H__
+
+namespace ActOfRose
+{
+	namespace Keyword
+	{
+		// Keyword classes
+		enum class EKeywords
+		{
+			EK_Var,				// "var" keyword
+			EK_Const,			// "const" keyword
+			EK_Func,			// "func" keyword
+			EK_If,				// "if" keyword
+			EK_Elif,			// "elif" keyword
+			EK_Else,			// "else" keyword
+			EK_For,				// "for" keyword
+			EK_While,			// "while" keyword
+			EK_Do				// "do" keyword
+		};
+
+	} // !namespace Keyword
+} // !namespace ActOfRose
+
+#endif // !__ACT_OF_ROSE_KEYWORDS_H__
