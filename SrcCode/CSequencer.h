484fc55c74b1aa4ca67147bedddbb486104ff411
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-21 00:04:23 +0300

    ADD: Class for analysing seq.s of tokens and determining the context

---
+++ SrcCode/CSequencer.h
@@ -0,0 +1,28 @@
+/*
+	Source code of ActOfRose global script execution system. Viktoriia Pashchenko. 2025.
+	
+	This file is part of system source code.
+	
+	@Name:			CSequencer.h
+	@Created:		20.03.2025
+	@Programmer:	Viktoriia Pashchenko (SweetyDarkyRosy)
+	
+	Class for analysing sequences of tokens and determining the context. */
+
+#ifndef __ACT_OF_ROSE_SEQUENCER_CLASS_H__
+#define __ACT_OF_ROSE_SEQUENCER_CLASS_H__
+
+namespace ActOfRose
+{
+	// Class for analysing sequences of tokens and determining the context
+	class CSequencer
+	{
+	public:
+		// Constructor
+		CSequencer() {}
+
+	};
+
+} // !namespace ActOfRose
+
+#endif // !__ACT_OF_ROSE_SEQUENCER_CLASS_H__
