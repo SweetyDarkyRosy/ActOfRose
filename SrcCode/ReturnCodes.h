6a30599f0e2251574d18396fbad9429f7e35e730
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-23 16:13:04 +0300

    ADD: AOR_CONTEXT_CREATE return code

--- SrcCode/ReturnCodes.h
+++ SrcCode/ReturnCodes.h
@@ -17,6 +17,7 @@
 #define AOR_TOKEN_END_OF_SCRIPT								1			// Code returned if the end of a script is reached
 #define AOR_CONTEXT_COMPLETE								2			// Code returned to indicate that current context is complete
 #define AOR_CONTEXT_EXECUTE									3			// Code returned to indicate that current context is ready for execution
+#define AOR_CONTEXT_CREATE									4			// Code returned to indicate that a new context should be created
 
 #define AOR_ERROR_INVALID_PARAMETER							(-1)		// Error due to invalid parameter/parameters
 #define AOR_ERROR_INVALID_CHARACTER							(-2)		// Error due to invalid character (according to actual character/string encoding)
