20d65b0a004e76faee58a5a6d11340c4ea204072
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-24 08:17:49 +0300

    ADD: AOR_ERROR_CONTEXT_DETERMINATION_ERROR error code

--- SrcCode/ReturnCodes.h
+++ SrcCode/ReturnCodes.h
@@ -27,5 +27,6 @@
 #define AOR_ERROR_TOKEN_INVALID_NUMBER_VALUE				(-6)		// Error due to invalid number value
 #define AOR_ERROR_TOKEN_INVALID_ENDING_BLOCK_DELIMITER		(-7)		// Error due to invalid ending block delimiter encountered
 #define AOR_ERROR_TOKEN_PREMATURE_END_OF_SCRIPT				(-8)		// Error due to premature end of script
+#define AOR_ERROR_CONTEXT_DETERMINATION_ERROR				(-9)		// Error due to the fact that the context could not be determined
 
 #endif // !__ACT_OF_ROSE_ERROR_CODES_H__
