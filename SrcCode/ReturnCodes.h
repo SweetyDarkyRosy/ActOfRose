32a59b7180090c24ee541ed8df33d7d199548ac8
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-24 20:38:50 +0300

    ADD: AOR_ERROR_INTERNAL_ERROR error code

--- SrcCode/ReturnCodes.h
+++ SrcCode/ReturnCodes.h
@@ -19,14 +19,15 @@
 #define AOR_CONTEXT_EXECUTE									3			// Code returned to indicate that current context is ready for execution
 #define AOR_CONTEXT_CREATE									4			// Code returned to indicate that a new context should be created
 
-#define AOR_ERROR_INVALID_PARAMETER							(-1)		// Error due to invalid parameter/parameters
-#define AOR_ERROR_INVALID_CHARACTER							(-2)		// Error due to invalid character (according to actual character/string encoding)
-#define AOR_ERROR_SCRIPT_FILE_NOT_LOADED					(-3)		// Error due to invalid path to a file with a script or internal error happened while opening a file
-#define AOR_ERROR_TOKEN_UNTERMINATED_STRING					(-4)		// Error due to unterminated/unquoted string when the end of a script is reached
-#define AOR_ERROR_TOKEN_INVALID_ESCAPE_SEQUENCE				(-5)		// Error due to invalid escape sequence found while string tokenisation
-#define AOR_ERROR_TOKEN_INVALID_NUMBER_VALUE				(-6)		// Error due to invalid number value
-#define AOR_ERROR_TOKEN_INVALID_ENDING_BLOCK_DELIMITER		(-7)		// Error due to invalid ending block delimiter encountered
-#define AOR_ERROR_TOKEN_PREMATURE_END_OF_SCRIPT				(-8)		// Error due to premature end of script
-#define AOR_ERROR_CONTEXT_DETERMINATION_ERROR				(-9)		// Error due to the fact that the context could not be determined
+#define AOR_ERROR_INTERNAL_ERROR							(-1)		// Error due to some internal error
+#define AOR_ERROR_INVALID_PARAMETER							(-2)		// Error due to invalid parameter/parameters
+#define AOR_ERROR_INVALID_CHARACTER							(-3)		// Error due to invalid character (according to actual character/string encoding)
+#define AOR_ERROR_SCRIPT_FILE_NOT_LOADED					(-4)		// Error due to invalid path to a file with a script or internal error happened while opening a file
+#define AOR_ERROR_TOKEN_UNTERMINATED_STRING					(-5)		// Error due to unterminated/unquoted string when the end of a script is reached
+#define AOR_ERROR_TOKEN_INVALID_ESCAPE_SEQUENCE				(-6)		// Error due to invalid escape sequence found while string tokenisation
+#define AOR_ERROR_TOKEN_INVALID_NUMBER_VALUE				(-7)		// Error due to invalid number value
+#define AOR_ERROR_TOKEN_INVALID_ENDING_BLOCK_DELIMITER		(-8)		// Error due to invalid ending block delimiter encountered
+#define AOR_ERROR_TOKEN_PREMATURE_END_OF_SCRIPT				(-9)		// Error due to premature end of script
+#define AOR_ERROR_CONTEXT_DETERMINATION_ERROR				(-10)		// Error due to the fact that the context could not be determined
 
 #endif // !__ACT_OF_ROSE_ERROR_CODES_H__
