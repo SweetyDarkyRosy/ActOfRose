5d7a7732a9744c419cddd392fe1c613a7fcfa5a9
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-24 23:41:53 +0300

    ADD: AOR_ERROR_TOKEN_UNEXPECTED_TOKEN error code

--- SrcCode/ReturnCodes.h
+++ SrcCode/ReturnCodes.h
@@ -23,12 +23,13 @@
 #define AOR_ERROR_INVALID_PARAMETER							(-2)		// Error due to invalid parameter/parameters
 #define AOR_ERROR_INVALID_CHARACTER							(-3)		// Error due to invalid character (according to actual character/string encoding)
 #define AOR_ERROR_SCRIPT_FILE_NOT_LOADED					(-4)		// Error due to invalid path to a file with a script or internal error happened while opening a file
-#define AOR_ERROR_TOKEN_UNTERMINATED_STRING					(-5)		// Error due to unterminated/unquoted string when the end of a script is reached
-#define AOR_ERROR_TOKEN_INVALID_ESCAPE_SEQUENCE				(-6)		// Error due to invalid escape sequence found while string tokenisation
-#define AOR_ERROR_TOKEN_INVALID_NUMBER_VALUE				(-7)		// Error due to invalid number value
-#define AOR_ERROR_TOKEN_INVALID_ENDING_BLOCK_DELIMITER		(-8)		// Error due to invalid ending block delimiter encountered
-#define AOR_ERROR_TOKEN_PREMATURE_END_OF_SCRIPT				(-9)		// Error due to premature end of script
-#define AOR_ERROR_TOKEN_IDENTIFIER_EXPECTED					(-10)		// Error due to that identifier was expected while tokenisation
-#define AOR_ERROR_CONTEXT_DETERMINATION_ERROR				(-11)		// Error due to the fact that the context could not be determined
+#define AOR_ERROR_TOKEN_UNEXPECTED_TOKEN					(-5)		// Error due to unexpected token encountered
+#define AOR_ERROR_TOKEN_UNTERMINATED_STRING					(-6)		// Error due to unterminated/unquoted string when the end of a script is reached
+#define AOR_ERROR_TOKEN_INVALID_ESCAPE_SEQUENCE				(-7)		// Error due to invalid escape sequence found while string tokenisation
+#define AOR_ERROR_TOKEN_INVALID_NUMBER_VALUE				(-8)		// Error due to invalid number value
+#define AOR_ERROR_TOKEN_INVALID_ENDING_BLOCK_DELIMITER		(-9)		// Error due to invalid ending block delimiter encountered
+#define AOR_ERROR_TOKEN_PREMATURE_END_OF_SCRIPT				(-10)		// Error due to premature end of script
+#define AOR_ERROR_TOKEN_IDENTIFIER_EXPECTED					(-11)		// Error due to that identifier was expected while tokenisation
+#define AOR_ERROR_CONTEXT_DETERMINATION_ERROR				(-12)		// Error due to the fact that the context could not be determined
 
 #endif // !__ACT_OF_ROSE_ERROR_CODES_H__
