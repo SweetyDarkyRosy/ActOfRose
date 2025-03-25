c4f164559a5a67b6bcafed07ea9cd6f7113f713d
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-25 15:51:41 +0300

    ADD: AOR_ERROR_EXEC_IDENTIFIER_ALREADY_IN_USE error code

--- SrcCode/ReturnCodes.h
+++ SrcCode/ReturnCodes.h
@@ -31,5 +31,6 @@
 #define AOR_ERROR_TOKEN_PREMATURE_END_OF_SCRIPT				(-10)		// Error due to premature end of script
 #define AOR_ERROR_TOKEN_IDENTIFIER_EXPECTED					(-11)		// Error due to that identifier was expected while tokenisation
 #define AOR_ERROR_CONTEXT_DETERMINATION_ERROR				(-12)		// Error due to the fact that the context could not be determined
+#define AOR_ERROR_EXEC_IDENTIFIER_ALREADY_IN_USE			(-13)		// Error due to that a given identifier is already in use
 
 #endif // !__ACT_OF_ROSE_ERROR_CODES_H__
