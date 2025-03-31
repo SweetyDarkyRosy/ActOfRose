6c304d926b0007c42e9082e72de2456c5ef7ad07
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-03-31 10:24:30 +0300

    ADD: AOR_ERROR_EXEC_UNSUPPORTED_OPERATION error code

--- SrcCode/ReturnCodes.h
+++ SrcCode/ReturnCodes.h
@@ -33,5 +33,6 @@
 #define AOR_ERROR_CONTEXT_DETERMINATION_ERROR				(-12)		// Error due to the fact that the context could not be determined
 #define AOR_ERROR_EXEC_IDENTIFIER_ALREADY_IN_USE			(-13)		// Error due to that a given identifier is already in use
 #define AOR_ERROR_EXEC_NO_OPERATOR_CHILD					(-14)		// Error due to that an operator node has no child operand nodes
+#define AOR_ERROR_EXEC_UNSUPPORTED_OPERATION				(-15)		// Error due to unsupported operation
 
 #endif // !__ACT_OF_ROSE_ERROR_CODES_H__
