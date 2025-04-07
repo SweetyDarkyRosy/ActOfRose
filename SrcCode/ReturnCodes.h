3048757b3f563694ccd910ca01a0906ca06aea84
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-04-08 00:55:29 +0300

    ADD: AOR_ERROR_EXEC_INVALID_EXPRESSION error code

--- SrcCode/ReturnCodes.h
+++ SrcCode/ReturnCodes.h
@@ -37,5 +37,6 @@
 #define AOR_ERROR_EXEC_NON_VALUE_TOKEN						(-16)		// Error due to trying to retrieve a value from a non-value token
 #define AOR_ERROR_EXEC_EXPRESSION_EXPECTED					(-17)		// Error due to that expression was expected
 #define AOR_ERROR_EXEC_UNDECLARED_IDENTIFIER				(-18)		// Error due to undeclared identifier
+#define AOR_ERROR_EXEC_INVALID_EXPRESSION					(-19)		// Error due to an invalid expression
 
 #endif // !__ACT_OF_ROSE_ERROR_CODES_H__
