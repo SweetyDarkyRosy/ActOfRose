cf1276201239ac19c458540dc196a57890757d90
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-04-08 00:53:46 +0300

    CHG: Tokenisation of unary minus and plus op.s near numbers was changed

--- SrcCode/CLexer.cpp
+++ SrcCode/CLexer.cpp
@@ -124,24 +124,7 @@ int ActOfRose::CLexer::RetrieveNextToken(ActOfRose::Token::SToken* newToken)
 			}
 			else if (IsOperator(retrievedChar) == true)
 			{
-				if ((retrievedChar == '+') || (retrievedChar == '-'))
-				{
-					retrievedChar = _pScriptStream->get();
-					newToken->value = retrievedChar;
-
-					if (IsDigit(_pScriptStream->peek()) == true)
-					{
-						result = RetrieveNumberToken(newToken);
-					}
-					else
-					{
-						result = RetrieveOperatorToken(newToken);
-					}
-				}
-				else
-				{
-					result = RetrieveOperatorToken(newToken);
-				}
+				result = RetrieveOperatorToken(newToken);
 			}
 			else if (retrievedChar == '.')
 			{
