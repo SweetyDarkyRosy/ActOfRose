aafeef75362061600f33ea1819e4ba4bbcc5d866
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-04-12 21:50:44 +0300

    ADD: Class of preprocessor for extracting predefined values

---
+++ SrcCode/CPreProcessor.h
@@ -0,0 +1,28 @@
+/*
+	Source code of ActOfRose global script execution system. Viktoriia Pashchenko. 2025.
+	
+	This file is part of system source code.
+	
+	@Name:			CPreProcessor.h
+	@Created:		12.04.2025
+	@Programmer:	Viktoriia Pashchenko (SweetyDarkyRosy)
+	
+	Class of preprocessor for extracting predefined values from a cache file and from a command line. */
+
+#ifndef __ACT_OF_ROSE_PREPROCESSOR_CLASS_H__
+#define __ACT_OF_ROSE_PREPROCESSOR_CLASS_H__
+
+namespace ActOfRose
+{
+	// Class of preprocessor for extracting predefined values from a cache file and from a command line
+	class CPreProcessor
+	{
+	public:
+		// Constructor
+		CPreProcessor() {}
+
+	};
+
+} // !namespace ActOfRose
+
+#endif // !__ACT_OF_ROSE_PREPROCESSOR_CLASS_H__
