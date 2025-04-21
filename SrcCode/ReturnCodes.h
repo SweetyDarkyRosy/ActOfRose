db32a8c15017f3c8944e5a9dfe73bffa50b1a198
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-04-21 23:16:41 +0300

    ADD: AOR_LEAVE_EXECUTION return code

--- SrcCode/ReturnCodes.h
+++ SrcCode/ReturnCodes.h
@@ -19,6 +19,7 @@
 #define AOR_CONTEXT_COMPLETE								3			// Code returned to indicate that current context is complete
 #define AOR_CONTEXT_EXECUTE									4			// Code returned to indicate that current context is ready for execution
 #define AOR_CONTEXT_CREATE									5			// Code returned to indicate that a new context should be created
+#define AOR_LEAVE_EXECUTION									6			// Code returned to indicate that current subroutine (execution process) should be left
 
 #define AOR_ERROR_INTERNAL_ERROR							(-1)		// Error due to some internal error
 #define AOR_ERROR_INVALID_ARG_NUMBER						(-2)		// Error due to some internal error
