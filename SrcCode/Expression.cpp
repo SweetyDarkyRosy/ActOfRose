e7fd4bef93411ea222885078a023e89668976cc1
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-04-26 20:30:49 +0300

    FIX: Working with right child while operation has been fixed

--- SrcCode/Expression.cpp
+++ SrcCode/Expression.cpp
@@ -170,21 +170,6 @@ int ActOfRose::AST::CExprASTOperatorNode::RetrieveValue(ActOfRose::Value::SValue
 
 			return valueRetrievingResult;
 		}
-
-		if (rightValueRef.category == ActOfRose::Value::EValueCategories::EVC_None)
-		{
-			if (leftValueRef.category == ActOfRose::Value::EValueCategories::EVC_PRValue)
-			{
-				delete leftValueRef.value.value;
-			}
-
-			return valueRetrievingResult;
-
-			ActOfRose::WriteLog(PREF_STRING("Child node of operator returned Null value"),
-			(sizeof(PREF_STRING("Child node of operator returned Null value")) / sizeof(PChar)), ActOfRose::ELogLevel::ELL_Error);
-
-			return AOR_ERROR_EXEC_INITIAL_VALUE_REQUIRED;
-		}
 	}
 
 	int execResult = ActOfRose::Operation::ExecuteOperation(valueRefHolder, _mOperationType, &leftValueRef, &rightValueRef);
