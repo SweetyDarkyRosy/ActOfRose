61d1d10241e83917aa4672dede06dd6f53feb230
Viktoriia Pashchenko <sweetydarkyrosy@gmail.com>
2025-05-01 15:09:02 +0300

    ADD: boolean / integer operation

--- SrcCode/Value/CBooleanValue.cpp
+++ SrcCode/Value/CBooleanValue.cpp
@@ -211,6 +211,16 @@ int ActOfRose::Value::CBooleanValue::ExecuteOperation(ActOfRose::Value::SValueRe
 					break;
 				}
 
+				case ActOfRose::Value::EValueType::EVT_Integer:
+				{
+					ActOfRose::Value::CIntegerValue* rightIntValue = (ActOfRose::Value::CIntegerValue*)rightValue;
+					int intResult = (int)_mValue / rightIntValue->GetRawValue();
+
+					retValueRefHolder->value.value = new ActOfRose::Value::CIntegerValue(intResult);
+
+					break;
+				}
+
 				default:
 				{
 					return AOR_ERROR_EXEC_UNSUPPORTED_OPERATION;
